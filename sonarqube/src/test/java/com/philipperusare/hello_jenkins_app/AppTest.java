0bc5349b8c679666bbc3678e96f236d9fa9cd86a
philippe-rusare-dev <philipperusare0@gmail.com>
2026-08-01 12:58:38 +0200

    save project

--- sonarqube/src/test/java/com/philipperusare/hello_jenkins_app/AppTest.java
+++ sonarqube/src/test/java/com/philipperusare/hello_jenkins_app/AppTest.java
@@ -2,31 +2,30 @@ package com.philipperusare.hello_jenkins_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(App.class)
 class AppTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void testShowHomePage() throws Exception {
+    void shouldReturnHomePage() throws Exception {
 
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(model().attribute(
                         "title",
-                        "I HAVE DEPLOY MY FIRST APP WITH JENKIN 😁😁"));
+                        equalTo("I HAVE DEPLOY MY FIRST APP WITH JENKIN 😁😁")
+                ));
 
     }
+
 }
