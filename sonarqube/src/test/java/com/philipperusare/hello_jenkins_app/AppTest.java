ddffb524bc77190fa603d7bc186d1e708ba58d62
philippe-rusare-dev <philipperusare0@gmail.com>
2026-08-01 12:51:31 +0200

    save project

--- sonarqube/src/test/java/com/philipperusare/hello_jenkins_app/AppTest.java
+++ sonarqube/src/test/java/com/philipperusare/hello_jenkins_app/AppTest.java
@@ -2,28 +2,29 @@ package com.philipperusare.hello_jenkins_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-// 👇 Voici le nouvel import valide pour Spring Boot 4.1.0
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AppTest {
+class AppTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testShowHomePage() throws Exception {
+    void testShowHomePage() throws Exception {
+
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
-                .andExpect(model().attribute("title", "I HAVE DEPLOY MY FIRST APP WITH JENKIN 😁😁"));
+                .andExpect(model().attribute(
+                        "title",
+                        "I HAVE DEPLOY MY FIRST APP WITH JENKIN 😁😁"));
+
     }
 }
